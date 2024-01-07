d96a7ec0a0d4db09c7cb312736451fd93f454be8
beearion <153745380+beearion@users.noreply.github.com>
2024-01-08 01:21:00 +0300

    Update vilo.js

--- vilo.js
+++ vilo.js
@@ -2,7 +2,7 @@ let x = 123;
 document.getElementById("demo").innerHTML =
   x.toString() + "<br>" +
    (123).toString() + "<br>" +
-   (100 + 2322).toString();
+   (100 + 23227).toString();
 
 document.getElementById("demo").innerHTML =
   x.toString() + "<br>" +
