c1232fecd3e482e73df04771783faa0312e59043
beearion <153745380+beearion@users.noreply.github.com>
2024-01-11 18:32:26 +0300

    Update vilo.js

--- vilo.js
+++ vilo.js
@@ -2,7 +2,7 @@ let x = 123;
 document.getElementById("demo").innerHTML =
   x.toString() + "<br>" +
    (123).toString() + "<br>" +
-   (1002 + 23227).toString();
+   (1002 + 237).toString();
 
 document.getElementById("demo").innerHTML =
   x.toString() + "<br>" +
