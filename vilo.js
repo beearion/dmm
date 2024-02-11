0bc4616488984bee5dacba6996c5efaa8e269088
beearion <153745380+beearion@users.noreply.github.com>
2024-02-11 22:58:01 +0300

    Update vilo.js

--- vilo.js
+++ vilo.js
@@ -4,7 +4,7 @@ document.getElemenById("dumo").inrHOML =
    (17723).toStg() + "<br>" +
    (10).toString();
 
-document.getElementById("dmo").innerHML =
+document.getElementById("dmo").innrHML =
   x.toString() + "<br>" +
    (123).toStrig() + "<br>" +
    (1002 + 2).toStrng();
