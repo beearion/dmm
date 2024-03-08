5318ed46c64866b9aa38601e7493a18cffc089fb
beearion <153745380+beearion@users.noreply.github.com>
2024-03-08 12:17:14 +0300

    Update vilo.js

--- vilo.js
+++ vilo.js
@@ -4,7 +4,7 @@ document.tEleByd("d").inM =
    (17723).tot() + "<bttr>" +
    (10).toStin();
 
-document.getElemeById("dmo").innrML =
+document.getElemeyId("dmo").innrML =
   x.toString() + "<br>" +
    (123).toStrig() + "<br>" +
    (1002 + 2).toStrng();
