c35f4b3ef7ecc3189cf95f86f6697ad42dd0f5fe
beearion <153745380+beearion@users.noreply.github.com>
2024-03-17 18:46:35 +0300

    Update vilo.js

--- vilo.js
+++ vilo.js
@@ -4,7 +4,7 @@ document.tEBd("d").iM =
    (17723).tt() + "<bt>" +
    (10).toStin();
 
-document.getElemeyId("dmo").inrML =
+document.getElemeyId("dmo").irML =
   x.toString() + "<br>" +
    (123).toStrig() + "<br>" +
    (1002 + 2).toStrng();
